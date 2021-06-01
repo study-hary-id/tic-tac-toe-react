9c83728bbbd05558cc96731290f49c3634f0f1e3
study-hary-id <haryansyah.study.id@gmail.com>
2021-06-01 15:01:35 +0700

    Create tic-tac-toe react project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Board from './components/Board';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Board />
+      </header>
+    </div>
+  );
+}
+
+export default App;
