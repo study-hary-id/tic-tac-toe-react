4e6850ca274626d1baf23d687a607c1565cc019e
study-hary-id <haryansyah.study.id@gmail.com>
2021-06-02 15:16:16 +0700

    Format files with prettier

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,10 +5,10 @@ class Board extends React.Component {
   renderSquare(i) {
     return <Square value={i} />;
   }
-  
+
   render() {
     const status = 'Next player : X';
-    
+
     return (
       <>
         <div className="status">{status}</div>
