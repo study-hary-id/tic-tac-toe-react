f928e6b927c57a02012de45cbd0ca832c9516319
study-hary-id <haryansyah.study.id@gmail.com>
2021-06-04 11:05:12 +0700

    Add utils/calculateWinner.js

---
+++ src/utils/calculateWinner.js
@@ -0,0 +1,29 @@
+/**
+ * Return the winner from conditions that have provided.
+ * @param {Array<any>} squares value of square components.
+ * @returns {String} X value or O value.
+ */
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i of lines) {
+    const [x, y, z] = i;
+
+    if (squares[x] && squares[x] === squares[y] && squares[x] === squares[z]) {
+      return squares[x];
+    }
+  }
+
+  return null;
+}
+
+export default calculateWinner;
